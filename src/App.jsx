3863988d7e67ccb3019394deed408e81517f3ad2
Eduardo Tonatto <edtonatto@gmail.com>
2024-02-17 14:44:13 -0300

    :sparkles: feat: add search bar and call OMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-			"Title": "Harry Potter and the Deathly Hallows: Part 2",
-			"Year": "2011",
-			"imdbID": "tt1201607",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BMGVmMWNiMDktYjQ0Mi00MWIxLTk0N2UtN2ZlYTdkN2IzNDNlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Sorcerer's Stone",
-			"Year": "2001",
-			"imdbID": "tt0241527",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BNmQ0ODBhMjUtNDRhOC00MGQzLTk5MTAtZDliODg5NmU5MjZhXkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Chamber of Secrets",
-			"Year": "2002",
-			"imdbID": "tt0295297",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BMjE0YjUzNDUtMjc5OS00MTU3LTgxMmUtODhkOThkMzdjNWI4XkEyXkFqcGdeQXVyMTA3MzQ4MTc0._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Prisoner of Azkaban",
-			"Year": "2004",
-			"imdbID": "tt0304141",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BMTY4NTIwODg0N15BMl5BanBnXkFtZTcwOTc0MjEzMw@@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Goblet of Fire",
-			"Year": "2005",
-			"imdbID": "tt0330373",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BMTI1NDMyMjExOF5BMl5BanBnXkFtZTcwOTc4MjQzMQ@@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Order of the Phoenix",
-			"Year": "2007",
-			"imdbID": "tt0373889",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BOTA3MmRmZDgtOWU1Ny00ZDc5LWFkN2YtNzNlY2UxZmY0N2IyXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Deathly Hallows: Part 1",
-			"Year": "2010",
-			"imdbID": "tt0926084",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Half-Blood Prince",
-			"Year": "2009",
-			"imdbID": "tt0417741",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter 20th Anniversary: Return to Hogwarts",
-			"Year": "2022",
-			"imdbID": "tt16116174",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BNTZkNWEyZTgtYzJlOS00OWNiLTgwZjMtZGU5NTRhNDNjOTRhXkEyXkFqcGdeQXVyNjk1Njg5NTA@._V1_SX300.jpg"
-		},
-		{
-			"Title": "Harry Potter and the Forbidden Journey",
-			"Year": "2010",
-			"imdbID": "tt1756545",
-			"Type": "movie",
-			"Poster": "https://m.media-amazon.com/images/M/MV5BNDM0YzMyNGUtMTU1Yy00OTE2LWE5NzYtZDZhMTBmN2RkNjg3XkEyXkFqcGdeQXVyMzU5NjU1MDA@._V1_SX300.jpg"
-		}
-  ]);
-  
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getMoviesRequest = async (searchValue) => {
+    const url = `${process.env.MOVIES_APP_OMBD_API_URL}?s=${searchValue}&apikey=${process.env.MOVIES_APP_OMDB_API_KEY}`;
+    console.log('a')
+    console.log(searchValue)
+    console.log(process.env.MOVIES_APP_OMBD_API_URL)
+    const response = await (await fetch(url)).json();
+
+    if(response.Search)
+      setMovies(response.Search);
+  }
+
+  useEffect(() => {
+    getMoviesRequest(searchValue);
+  }, [searchValue]);
+
   return (
     <div className="container-fluid movies-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies"/>
+        <SearchBox setSearchValue={setSearchValue}/>
+      </div>
       <div className="row">
         <MovieList movies={movies} />
       </div>
